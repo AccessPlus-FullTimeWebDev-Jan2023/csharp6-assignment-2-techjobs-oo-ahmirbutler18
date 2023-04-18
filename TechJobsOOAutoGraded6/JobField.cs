cd62df9437bb8913e14ce1eb734af30033e9d318
Ahmir <ahmirbutler18@gmail.com>
2023-04-17 21:56:55 -0500

    Finish all task and completed the assignment

---
+++ TechJobsOOAutoGraded6/JobField.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TechJobsOOAutoGraded6
+{
+    public abstract class JobField
+    {
+        public int Id { get; }
+        private static int nextId = 1;
+        public string Value { get; set; }
+
+        public JobField()
+        {
+            Id = nextId;
+            nextId++;
+        }
+
+        public JobField(string value) : this()
+        {
+            Value = value;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null || GetType() != obj.GetType())
+            {
+                return obj is JobField type &&
+                   Id == type.Id;
+            }
+
+            JobField other = (JobField)obj;
+            return Id == other.Id;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Id);
+        }
+
+        public override string ToString()
+        {
+            return Value;
+        }
+    }
+
+}
+
+ 
